867f0a69a597679ccd0af30571b63dd8c9f0c985
Jakub <jakubkrys@gmail.com>
2021-12-06 21:28:50 +0100

    #13 poprawienie mapowania addPArty z get na post

--- src/main/java/pl/szymonleyk/spotifypartyapp/controller/HomeController.java
+++ src/main/java/pl/szymonleyk/spotifypartyapp/controller/HomeController.java
@@ -1,7 +1,7 @@
 package pl.szymonleyk.spotifypartyapp.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -17,7 +17,7 @@ public class HomeController {
         return "home.html";
     }
 
-    @GetMapping("/party-add")
+    @PostMapping("/party-add")
     public String addParty() {
         return "party-add.html";
     }
